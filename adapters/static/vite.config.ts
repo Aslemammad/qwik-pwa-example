917e492fb21ce836bede94f40f78d936b012825b
AmirSa12 <amirhosseinpr184@gmail.com>
2024-01-13 22:10:51 +0330

    test

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://qwik-pwa-example-32h.pages.dev",
+        origin: "https://qwik-pwa-example-32h.pages.dev/demo/todolist",
       }),
     ],
   };
