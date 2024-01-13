c85d895dc589f6df26dd4de66d428dccd3065e08
AmirSa12 <amirhosseinpr184@gmail.com>
2024-01-13 21:53:51 +0330

    fix http request bug

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://yoursite.qwik.dev",
+        origin: "https://qwik-pwa-example-32h.pages.dev/",
       }),
     ],
   };
